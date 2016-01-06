19055c6595135c887b6a0ef5f477a007f7387535
Evelyn Utterson <e.utterson@hotmail.co.uk>
2016-01-06 14:28:37 +0000

    added form for new and edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,13 @@
 class ArticlesController < ApplicationController
+  
+  before_action :load_article, except:[:index, :new, :create]
+
   def index
     @articles = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def create
@@ -16,8 +20,21 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    @article.update(article_params)
+    redirect_to(articles_path)
   end
 
   def destroy
+    @article.destroy
+    redirect_to(articles_path)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def load_article
+    @article = Article.find(params[:id]) 
   end
 end
