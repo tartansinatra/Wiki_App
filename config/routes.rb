1bfccdcb43e11898224830a230bb23d910c34245
Evelyn Utterson <e.utterson@hotmail.co.uk>
2016-01-06 11:51:31 +0000

    added associations and seeded data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  root to: 'home#index'
+  devise_for :users
+  # get 'articles/index'
+  resources :articles
+  root to: 'articles#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+User.create!(name: 'A', email: 'a@mail.com', password: 'password')
+
+Article.delete_all
+
+Article.create!(title: 'Article 1', content: 'Stuff')
+Article.create!(title: 'Article 2', content: 'More Stuff')
+Article.create!(title: 'Article 3', content: 'Different Stuff')
+Article.create!(title: 'Article 4', content: 'Extra Stuff')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
