2b9ca6f4486171f55514d3fe41d790098586cf3e
Evelyn Utterson <e.utterson@hotmail.co.uk>
2016-01-06 14:04:47 +0000

    updated controllers and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,23 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
